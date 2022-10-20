5ad6dcc296d7efdea50bbcff8fd1f8e4efcd8517
Roman <sholomitskyR@yandex.by>
2022-10-20 19:36:29 +0300

    feat: upd button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,22 +2,33 @@ import { FC, ReactNode } from 'react';
 
 import { ReactComponent } from '*.svg';
 
-import { Headline } from '@/components/Headline';
 import { Icon } from '@/components/Icon';
 
-import { Button } from './components.styled';
+import { Button } from './styled';
+
+export type ButtonType =
+  | 'fill'
+  | 'withIcon'
+  | 'withIconNoFill'
+  | 'withIconNoFillBig'
+  | 'little'
+  | 'circle'
+  | 'circle-mobile'
+  | 'search'
+  | 'outlined';
 
 interface ButtonProps {
   children: ReactNode;
   icon?: typeof ReactComponent;
   disabled?: boolean;
+  variant: ButtonType;
 }
 
-export const Buttons: FC<ButtonProps> = ({ children, icon, disabled }) => {
+export const Buttons: FC<ButtonProps> = ({ children, icon, disabled, variant }) => {
   return (
-    <Button disabled={disabled}>
+    <Button variant={variant} disabled={disabled}>
       {icon && <Icon icon={icon} />}
-      <Headline as={'h6'}>{children}</Headline>
+      {children}
     </Button>
   );
 };
---
+++ src/components/Button/styled.ts
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+
+import { ButtonType } from '.';
+
+import { ReactComponent } from '*.svg';
+
+type ButtonPropsType = {
+  contained?: boolean;
+  icon?: typeof ReactComponent;
+  variant: ButtonType;
+};
+
+export const Button = styled.button<ButtonPropsType>`
+  width: 190px;
+  height: 54px;
+  padding: 0;
+  background-color: ${(props) => props.theme.colors.primary};
+  color: white;
+  border-radius: 6px;
+  outline: 0;
+  border: 0;
+  box-shadow: 0px 12px 30px rgba(24, 92, 255, 0.18);
+  transition: 0.3s;
+  display: flex;
+  align-items: center;
+  justify-content: ${({ icon }) => (icon ? 'center' : 'space-around')};
+  font: ${(props) => props.theme.font.headline.headline6};
+
+  ${(props) => {
+    switch (props.variant) {
+      case 'fill':
+        return '';
+      case 'withIcon':
+        return `
+        width: 168px;
+        height: 45px;
+        padding: 10px 14px;
+        box-shadow: none;
+        font: ${props.theme.font.headline.headline7};`;
+      case 'circle':
+        return `
+        width: 140px;
+        height: 140px;
+        border-radius: 100%;
+        border: 9px solid #185CFF;
+        box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.4);
+        font: ${props.theme.font.headline.headline7};
+
+        &:hover {
+          border: 9px solid #467dff;
+        }
+
+        &::before {
+          content: '';
+          position: absolute;
+          left: -20px;
+          top: -20px;
+          right: -20px;
+          bottom: -20px;
+          border: 10px solid white;
+          border-radius: 50%;
+        }
+          `;
+    }
+  }}
+
+  &:hover {
+    text-decoration: none;
+    background-color: #467dff;
+  }
+
+  &:active {
+    box-shadow: none;
+  }
+
+  &:disabled {
+    background-color: ${(props) => props.theme.colors.grey};
+    box-shadow: none;
+  }
+
+  svg {
+    width: 24px;
+    height: 24px;
+    fill: ${(props) => props.theme.colors.white};
+  }
+`;
